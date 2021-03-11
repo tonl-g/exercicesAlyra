ebb102954f8f8a812da68193c44a4d1d525ecb0c
tonl-g <tlygonneville.dev@gmail.com>
2021-03-11 16:02:11 +0100

    Modif

--- week3/exo-expressions-conditions/vote.js
+++ week3/exo-expressions-conditions/vote.js
@@ -13,17 +13,6 @@ if (canVote) {
   console.log('You can not vote')
 } */
 
-let age = 18
-let canVote = false
-let isFrench = true
-let isFranceResident = false
-if (age >= 18 && isFrench && !isFranceResident) {
-  canVote = true
-} else {
-  canVote = false
-}
-if (canVote) {
-  console.log('You can vote')
-} else {
-  console.log('You can not vote')
-}
+let age = 25
+let canVote = age >= 18 ? true : false
+console.log(`${canVote ? 'You can vote' : 'You can not vote'}`)
