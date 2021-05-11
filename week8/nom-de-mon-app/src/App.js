12b3e1bb8bd238a83fc42dffc390112ede988aee
tonl-g <tlygonneville.dev@gmail.com>
2021-05-11 11:10:41 +0200

    Add dir

---
+++ week8/nom-de-mon-app/src/App.js
@@ -0,0 +1,15 @@
+import School from "./components/School"
+
+function App() {
+  return (
+    <section className="container">
+        <h1 className="my-3 text-center">Ma liste des écoles à recommander</h1>
+        <div className="row">
+        <div className="col-md-6"></div>
+        <div className="col-md-6"></div>
+        </div>
+    </section>
+  );
+}
+
+export default App;
